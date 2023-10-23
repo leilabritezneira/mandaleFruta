07db6e5e0e36d589d93b8ebc6a5705f60fdc5673
Leila Britez Neira <103675851+leilabritezneira@users.noreply.github.com>
2023-10-23 19:25:13 +0200

    Agrego la solicitud a la base de datos desde el index.js

--- e-commerce/server/server.js
+++ e-commerce/server/server.js
@@ -7,14 +7,14 @@ const { Pool } = require('pg');
 
 // Configuración de la conexión a la base de datos PostgreSQL
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'verduleria', 
-  password: 'postgres',
-  port: 5432,
+    user: 'postgres',
+    host: 'localhost',
+    database: 'verduleria', 
+    password: 'postgres',
+    port: 5432,
 });
 
-// REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel
+// Configuración de mercadoPago
 mercadopago.configure({
     access_token: "TEST-9004424593507134-091521-ea1d59107b1ffab96f224c7c84a44823-17406307",
 });
@@ -26,6 +26,19 @@ app.use(cors());
 
 app.get("/", function () {
     path.resolve(__dirname, "..", "client", "index.html");
+});
+
+//Solicitud a la base de datos de PostgreSQL
+app.get("/productos", (req, res) => {
+    pool.query('SELECT * FROM productos', (error, result) => {
+        if (error) {
+            console.error('Error al obtener productos', error);
+            res.status(500).json({ error: 'Error al obtener productos' });
+        } else {
+            const productos = result.rows;
+            res.json(productos);
+        }
+    });
 });
 
 app.post("/create_preference", (req, res) => {
